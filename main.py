05614c6f202df200ae64a670a032957f08b6033f
Osman Safa Ozer <79766197+safaozerosman@users.noreply.github.com>
2023-03-10 01:20:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,86 @@
+print("Kodlamaio")
+baslik = "Taşıt Kredisi"
+print(baslik)
+#string => Metinsel İfade 
+baslik = "İhtiyaç Kredisi"
+print(baslik)
+#int, integer => tam sayı
+vade = 36
+ekVade = 6
+vade2 = "36"
+
+#float, decimal, double  => ondalıklı sayı
+aylikOdeme = 200.50
+
+
+#bool, boolean => True veya false
+yukselisteMi = True
+
+# Matematiksel Operatörler
+
+# +
+print(5 + 5)
+print(vade + 12)
+print(vade + ekVade)
+
+# -
+print(5-5)
+print(vade-12)
+print(vade-ekVade)
+
+# *
+print(5*5)
+print(vade*2)
+print(vade*ekVade)
+print(10*10)
+
+# /
+print(5 / 5)
+print(vade / 2)
+print(vade / ekVade)
+print(10 / 2)
+
+yeniVade = vade / 2
+fiyat = 100
+indirimliFiyat = fiyat - 20
+
+print(yeniVade)
+print(indirimliFiyat)
+
+
+# % => mod operator (bir sayının diğer sayıya bölümünden kalanı verir)
+
+print(10 % 2)
+print(vade % 3)
+print(vade % 5)
+print(vade % ekVade)
+
+
+# mantıksal operatorler
+
+# == 
+print(1 == 1)
+#eger dogruysa boolean true veya yanlışsa boolean false olarak cıktı verecektir
+print(1 == 2)
+
+# >
+print(2 > 1)
+print(1 > 3)
+print(1 > 1)
+
+# <
+print(2 > 1)
+print(1 > 3)
+print(1 > 1)
+
+# birden fazla satiri yorum satiri haline getirmek icin satirlar secilirse CTRL K + CTRL C 
+
+# >=
+print (1 >= 1)
+
+# <=
+print(1 < 3)
+
+# != (esit degildir)
+print(1 != 1)
+
